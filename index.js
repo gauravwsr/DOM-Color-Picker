b6f57bf7e2764641616388906583021d683b5050
gaurav <gauravwasekar7@gmail.com>
2024-10-25 13:23:59 +0530

    Color Picker Dom Project File added

---
+++ index.js
@@ -0,0 +1,22 @@
+let bgcolorbtn = document.getElementById("main-cont");
+let colorvl = document.getElementById("colorvl");
+
+function bgcolor(){
+    bgcolorbtn.style.backgroundColor = "#e0e0e0";
+    colorvl.textContent = "#e0e0e0";
+}
+
+function bgcolorg(){
+    bgcolorbtn.style.backgroundColor = "#6fcf97";
+    colorvl.textContent = "#6fcf97";
+}
+
+function bgcolorb(){
+    bgcolorbtn.style.backgroundColor = "#56ccf2";
+    colorvl.textContent = "#56ccf2";
+}
+
+function bgcolorv(){
+    bgcolorbtn.style.backgroundColor = "#bb6bd9";
+    colorvl.textContent = "#bb6bd9";
+}
